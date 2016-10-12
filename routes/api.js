9e1fb7795273f060c7d10f99c929851416732510
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-12 23:43:44 +0200

    added menu api endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// router.options('/banner', function(req, res, next) {
-//   res.header( 'Access-Control-Allow-Origin', '*');
-//   res.header( 'Access-Control-Allow-Credentials', true);
-//   res.header( 'Access-Control-Allow-Methods', 'GET');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
-
-/* GET home page. */
 router.get('/banner', function(req, res, next) {
   res.header( 'Access-Control-Allow-Origin', '*');
   res.header( 'Access-Control-Allow-Credentials', true);
@@ -60,6 +51,64 @@ router.post('/login', function(req, res, next){
     res.sendStatus(403);
   }
 
+});
+
+router.get('/menu', function(req, res, next){
+  var menu = {
+    config_menu: {
+      id: 'DEFAULT',
+      title: '',
+      items: [{
+          'action': 'accedo://page/resume',
+          'id': 'resume',
+          'state': '',
+          'title': 'Resume',
+          'items': []
+        },
+        {
+            'action': 'accedo://page/movies',
+            'id': 'movies',
+            'state': '',
+            'title': 'Movies',
+            'items': [{
+                  'action': 'accedo://page/movies/action',
+                  'id': 'movies-action',
+                  'state': '',
+                  'title': 'Action',
+                  'items': []
+            },{
+                    'action': 'accedo://page/movies/comedy',
+                    'id': 'movies-comedy',
+                    'state': '',
+                    'title': 'Comedy',
+                    'items': []
+            },{
+                    'action': 'accedo://page/movies/scifi',
+                    'id': 'movies-scifi',
+                    'state': '',
+                    'title': 'Sci-Fi',
+                    'items': []
+            }]
+        },
+        {
+            'action': 'accedo://page/series',
+            'id': 'series',
+            'state': '',
+            'title': 'Series',
+            'items': []
+        },
+        {
+            'action': 'accedo://page/search',
+            'id': 'search',
+            'state': '',
+            'title': 'Search',
+            'items': []
+          },
+      ]
+    }
+  };
+
+  res.send(menu);
 });
 
 module.exports = router;
