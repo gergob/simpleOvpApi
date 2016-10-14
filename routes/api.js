f1a2df1ac83c2968199a58fc61387348be589d0d
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-14 23:33:58 +0200

    added support for series, extended base object with type property and added series items

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,9 @@ var movies = [{
   title: 'Ant - Man',
   description: 'Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, plan and pull off a heist that will save the world.',
   genre: 'Sci-Fi/Action',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 2,
   logoSrc: 'images/logo_avengers.jpg',
@@ -16,7 +18,9 @@ var movies = [{
   title: 'Avengers',
   description: 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity.',
   genre: 'Sci-Fi/Action',
-  finished: true
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: true,
+  type: 'movie'
 }, {
   id: 3,
   logoSrc: 'images/logo_edgeoftomorrow.jpg',
@@ -24,7 +28,9 @@ var movies = [{
   title: 'Edge of Tomorrow',
   description: 'A soldier fighting aliens gets to relive the same day over and over again, the day restarting every time he dies.',
   genre: 'Sci-Fi/Action',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 4,
   logoSrc: 'images/logo_interstellar.jpg',
@@ -32,7 +38,9 @@ var movies = [{
   title: 'Interstellar',
   description: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
   genre: 'Sci-Fi',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 5,
   logoSrc: 'images/logo_johncarter.jpg',
@@ -40,7 +48,9 @@ var movies = [{
   title: 'John Carter',
   description: 'Transported to Barsoom, a Civil War vet discovers a barren planet seemingly inhabited by 12-foot tall barbarians. Finding himself prisoner of these creatures, he escapes, only to encounter Woola and a princess in desperate need of a savior.',
   genre: 'Sci-Fi',
-  finished: true
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: true,
+  type: 'movie'
 }, {
   id: 6,
   logoSrc: 'images/logo_captainamerica.jpg',
@@ -48,7 +58,9 @@ var movies = [{
   title: 'Captain America',
   description: 'Steve Rogers, a rejected military soldier transforms into Captain America after taking a dose of a "Super-Soldier serum". But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization.',
   genre: 'Sci-Fi',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 7,
   logoSrc: 'images/logo_deadpool.jpg',
@@ -56,7 +68,9 @@ var movies = [{
   title: 'Deadpool',
   description: 'A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.',
   genre: 'Sci-Fi',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 8,
   logoSrc: 'images/logo_fantastic4.jpg',
@@ -64,7 +78,9 @@ var movies = [{
   title: 'Fant4stic',
   description: 'Four young outsiders teleport to an alternate and dangerous universe which alters their physical form in shocking ways. The four must learn to harness their new abilities and work together to save Earth from a former friend turned enemy.',
   genre: 'Sci-Fi',
-  finished: true
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: true,
+  type: 'movie'
 }, {
   id: 9,
   logoSrc: 'images/logo_frozen.jpg',
@@ -72,7 +88,9 @@ var movies = [{
   title: 'Frozen',
   description: 'When the newly crowned Queen Elsa accidentally uses her power to turn things into ice to curse her home in infinite winter, her sister, Anna, teams up with a mountain man, his playful reindeer, and a snowman to change the weather condition.',
   genre: 'Animation',
-  finished: true
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: true,
+  type: 'movie'
 }, {
   id: 10,
   logoSrc: 'images/logo_ironman3.jpg',
@@ -80,7 +98,9 @@ var movies = [{
   title: 'Iron Man 3',
   description: 'When Tony Stark\'s world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.',
   genre: 'Sci-Fi',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 11,
   logoSrc: 'images/logo_rio.jpg',
@@ -88,7 +108,9 @@ var movies = [{
   title: 'Rio',
   description: 'When Blu, a domesticated macaw from small-town Minnesota, meets the fiercely independent Jewel, he takes off on an adventure to Rio de Janeiro with the bird of his dreams.',
   genre: 'Animation',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 12,
   logoSrc: 'images/logo_riseoftheguardians.jpg',
@@ -96,7 +118,9 @@ var movies = [{
   title: 'Rise of the Guardians',
   description: 'When the evil spirit Pitch launches an assault on Earth, the Immortal Guardians team up to protect the innocence of children all around the world.',
   genre: 'Animation',
-  finished: true
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: true,
+  type: 'movie'
 }, {
   id: 13,
   logoSrc: 'images/logo_startrek.jpg',
@@ -104,7 +128,9 @@ var movies = [{
   title: 'Star Trek',
   description: 'After the crew of the Enterprise find an unstoppable force of terror from within their own organization, Captain Kirk leads a manhunt to a war-zone world to capture a one-man weapon of mass destruction.',
   genre: 'Sci-Fi',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 14,
   logoSrc: 'images/logo_thehobbit.jpg',
@@ -112,7 +138,9 @@ var movies = [{
   title: 'The Hobbit',
   description: 'A reluctant hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home - and the gold within it - from the dragon Smaug.',
   genre: 'Sci-Fi',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 15,
   logoSrc: 'images/logo_theinterview.jpg',
@@ -120,7 +148,9 @@ var movies = [{
   title: 'The Interview',
   description: 'Dave Skylark and his producer Aaron Rapoport run the celebrity tabloid show "Skylark Tonight". When they land an interview with a surprise fan, North Korean dictator Kim Jong-un, they are recruited by the CIA to turn their trip to Pyongyang into an assassination mission.',
   genre: 'Action-Comedy',
-  finished: false
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'movie'
 }, {
   id: 16,
   logoSrc: 'images/logo_warcraft.jpg',
@@ -128,7 +158,79 @@ var movies = [{
   title: 'Warcraft',
   description: 'As an Orc horde invades the planet Azeroth using a magic portal, a few human heroes and dissenting Orcs must attempt to stop the true evil behind this war.',
   genre: 'Sci-Fi',
-  finished: true
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: true,
+  type: 'movie'
+}, {
+  id: 17,
+  logoSrc: 'images/logo_bigbangtheory.jpg',
+  imageSrc: 'images/banner_bigbangtheory.jpg',
+  title: 'Big Bang Theory',
+  description: 'A woman who moves into an apartment across the hall from two brilliant but socially awkward physicists shows them how little they know about life outside of the laboratory.',
+  genre: 'Comedy',
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'serie'
+}, {
+  id: 18,
+  logoSrc: 'images/logo_friends.jpg',
+  imageSrc: 'images/banner_friends.jpg',
+  title: 'Friends',
+  description: 'Follows the personal and professional lives of six 20 to 30-something yrs old friends living in Manhattan.',
+  genre: 'Comedy',
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'serie'
+}, {
+  id: 19,
+  logoSrc: 'images/logo_stargate.jpg',
+  imageSrc: 'images/banner_stargate.jpg',
+  title: 'Stargate',
+  description: 'A secret military team, SG-1, is formed to explore the recently discovered Stargates.',
+  genre: 'Sci-Fi',
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: true,
+  type: 'serie'
+}, {
+  id: 20,
+  logoSrc: 'images/logo_familyguy.jpg',
+  imageSrc: 'images/banner_familyguy.jpg',
+  title: 'Family Guy',
+  description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.',
+  genre: 'Comedy/Cartoon',
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'serie'
+}, {
+  id: 21,
+  logoSrc: 'images/logo_desperate.jpg',
+  imageSrc: 'images/banner_desperate.jpg',
+  title: 'Desperate Housewives',
+  description: 'Secrets and truths unfold through the lives of female friends in one suburban neighborhood, after the mysterious suicide of a neighbor.',
+  genre: 'Comedy',
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'serie'
+}, {
+  id: 22,
+  logoSrc: 'images/logo_lost.jpg',
+  imageSrc: 'images/banner_lost.jpg',
+  title: 'Lost',
+  description: 'The survivors of a plane crash are forced to work together in order to survive on a seemingly deserted tropical island.',
+  genre: 'Thriller',
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'serie'
+}, {
+  id: 23,
+  logoSrc: 'images/logo_talespin.jpg',
+  imageSrc: 'images/banner_talespin.jpg',
+  title: 'TaleSpin',
+  description: 'Baloo the Bear from the Jungle Book is cast as a 1930\'s Pacific Islands bush pilot.',
+  genre: 'Comedy',
+  videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+  finished: false,
+  type: 'serie'
 }];
 
 router.get('/banner', function(req, res, next) {
@@ -262,19 +364,26 @@ router.get('/resume', function(req, res, next) {
 });
 
 router.get('/movie', function(req, res, next) {
-  var elements = movies;
-  res.send({items: elements});
+    var elements = movies;
+    res.send({items: elements});
 });
 
-router.get('/movie/:id', function(req, res, next) {
+router.get('/detail/:id', function(req, res, next) {
     res.send({elements: []});
 });
 
 router.get('/serie', function(req, res, next) {
-    res.send({elements: []});
+    var elements = [];
+    for (var i = 0; i < movies.length; ++i) {
+      if (movies[i].type === 'serie') {
+        elements.push(movies[i]);
+      }
+    }
+
+    res.send({items: elements});
 });
 
-router.get('/search', function(req, res, next) {
+router.get('/search/:phrase', function(req, res, next) {
     res.send({elements: []});
 });
 
