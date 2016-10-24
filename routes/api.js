89e6e1e7e9d28758538d914531a6e49c831f1917
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-24 22:20:13 +0200

    added some apidoc annotation

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,18 @@ var router = express.Router();
 var dummyData = require('./dummyData');
 var movies = dummyData.movies;
 
-
+/**
+ * @api {get} /banner
+ * @apiVersion 0.5.0
+ * @apiName GetBanner
+ * @apiExample Example usage:
+ * curl http://localhost/banner/
+ *
+ * @apiSuccess {Number}   id            The ID of the banner item
+ * @apiSuccess {String}   imageSrc      The image source of the banner
+ * @apiSuccess {String}   alt           The alt value for the image
+ * @apiSuccess {String}   title         The title of the movie which is shown on te banner
+ */
 router.get('/banner', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
@@ -40,6 +51,17 @@ router.get('/banner', function(req, res, next) {
     res.send(bannerData);
 });
 
+
+/**
+ * @api {post} /login
+ * @apiVersion 0.5.0
+ * @apiName Login
+ * @apiExample Example usage:
+ * curl -d '{email:"john@doe.com", password:"secret"}' http://localhost/banner/
+ *
+ * @apiSuccess 200
+ * @apiError 403
+ */
 router.post('/login', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
@@ -57,6 +79,41 @@ router.post('/login', function(req, res, next) {
 
 });
 
+/**
+ * @api {get} /menu
+ * @apiVersion 0.5.0
+ * @apiName GetMenu
+ * @apiExample Example usage:
+ * curl http://localhost/menu/
+ *
+ * @apiSuccess {Number}   id            The ID of the menu item
+ * @apiSuccess {String}   title         The title of the movie which is shown on te banner
+ * @apiSuccess {Object[]} items         The elements defined under this menu item
+ *
+ * @apiSuccessExample Succes-Response:
+ * HTTP/1.1 200 OK
+ *     {
+ *        id: 'DEFAULT',
+ *        title: '',
+ *        items: [{
+ *          'action': 'accedo://page/resume',
+ *          'id': 'resume',
+ *          'state': '',
+ *          'title': 'Resume',
+ *          'page': 'Resume',
+ *          'icon': 'update',
+ *          'items': []
+ *        }, {
+ *          'action': 'accedo://page/movies',
+ *          'id': 'movies',
+ *          'state': '',
+ *          'title': 'Movies',
+ *          'page': 'Movies',
+ *          'icon': 'movie',
+ *          'items': []
+ *        }]
+ *    }
+ */
 router.get('/menu', function(req, res, next) {
     var menu = {
         config_menu: {
@@ -125,6 +182,38 @@ router.get('/menu', function(req, res, next) {
     res.send(menu);
 });
 
+/**
+ * @api {get} /resume
+ * @apiVersion 0.5.0
+ * @apiName GetResume
+ * @apiExample Example usage:
+ * curl http://localhost/resume/
+ *
+ * @apiSuccess {Number}   id            The ID of the movie/serie item
+ * @apiSuccess {String}   logoSrc       The path to the logo for the movie/serie
+ * @apiSuccess {String}   imageSrc      The path of the image for the movie/serie
+ * @apiSuccess {String}   title         The title of the movie/serie which is shown on te banner
+ * @apiSuccess {String}   description   Short text about the content of the movie/serie
+ * @apiSuccess {String}   genre         The genre of the movie/serie
+ * @apiSuccess {String}   videSrc       The path to playback of the movie/serie
+ * @apiSuccess {Boolean}  finished      Has the user finished wathing the movie/serie
+ * @apiSuccess {String}   type          The type of the asset, can be 'movie' or 'serie'
+ * @apiSuccessExample Succes-Response:
+ * HTTP/1.1 200 OK
+ *     [{
+ *         id: 1,
+ *         logoSrc: 'images/logo_antman.jpg',
+ *         imageSrc: 'images/banner_antman.jpg',
+ *         title: 'Ant - Man',
+ *         description: 'Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, plan and pull off a heist that will save the world.',
+ *         genre: 'Sci-Fi/Action',
+ *         videoSrc: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+ *         finished: false,
+ *         type: 'movie'
+ *       },...
+ *      ]
+ *
+ */
 router.get('/resume', function(req, res, next) {
     var result = [];
 
@@ -204,7 +293,7 @@ router.post('/search', function(req, res, next) {
         if (movie.title.toLowerCase().indexOf(keyword) >= 0 ||
             movie.description.toLowerCase().indexOf(keyword) >= 0 ||
             movie.genre.toLowerCase().indexOf(keyword) >= 0) {
-              console.log('Found entry matching search criteria, id:[' + movie.id + '] and title:[' + movie.title + ']')  
+              console.log('Found entry matching search criteria, id:[' + movie.id + '] and title:[' + movie.title + ']')
               result.push(movie);
         }
     });
